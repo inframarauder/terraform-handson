dc4672b51150d34e88d0f70e19ff47742524e852
subhasis020299 <subhasisdas125@gmail.com>
2021-11-15 22:02:06 +0530

    updatd sg cidr blocks to all sources

--- modules/networking/security_group.tf
+++ modules/networking/security_group.tf
@@ -8,8 +8,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.terraform_vpc.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.terraform_vpc.ipv6_cidr_block]
+    cidr_blocks      = [ "0.0.0.0/0" ]
   }
 
   egress {
