69269bcba0ffbd2b70e479e586fc95cf197fa9cc
subhasis020299 <subhasisdas125@gmail.com>
2021-11-15 20:02:36 +0530

    assigned public IP to ec2

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "terraform_ec2" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id = aws_subnet.terraform_subnet.id
+  associate_public_ip_address = true
 
   tags = {
     "Name" = "terraform_ec2"
