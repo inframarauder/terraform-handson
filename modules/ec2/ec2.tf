8568c21623b07713bf45e61dee3a5fc13e9aaad7
subhasis020299 <subhasisdas125@gmail.com>
2021-11-17 10:45:56 +0530

    separate datasource file

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,22 +1,3 @@
-#getting latest ubutnu ami id
-
-data "aws_ami" "ubuntu" {
-
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
 resource "aws_instance" "terraform_ec2" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
